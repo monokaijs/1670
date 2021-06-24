e66dbe71b9d46400fd6aad1fb16a2556c1e2252d
MonokaiJs <monokaijs@gmail.com>
2021-06-24 15:49:35 +0700

    Move build path

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ require("./routes/auth.route")(app);
 require("./routes/main.route")(app);
 
 app.get('*', (req,res) =>{
-  res.sendFile(path.join(__dirname, '..' , 'public/index.html'));
+  res.sendFile(path.join(__dirname, '..' , 'client/build/index.html'));
 });
 
 mongoose.connect(process.env.DB_CONNECTION_URL, {
