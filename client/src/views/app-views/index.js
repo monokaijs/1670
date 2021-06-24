bf49da1b9960121de0097c0cdd1884bfc55b6938
Nguyen Thu Thuy <haducc246@gmail.com>
2021-06-24 18:01:15 +0700

    Test push

--- client/src/views/app-views/index.js
+++ client/src/views/app-views/index.js
@@ -8,6 +8,7 @@ export const AppViews = () => {
     <Suspense fallback={<Loading cover="content"/>}>
       <Switch>
         <Route path={`${APP_PREFIX_PATH}/home`} component={lazy(() => import(`./home`))} />
+        <Route path={`${APP_PREFIX_PATH}/manage`} component={lazy(() => import(`./manage`))} />
         <Redirect from={`${APP_PREFIX_PATH}`} to={`${APP_PREFIX_PATH}/home`} />
       </Switch>
     </Suspense>
