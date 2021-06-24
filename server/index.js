41fb7b097a9101207c16711f78baa5bbfe117a12
MonokaiJs <monokaijs@gmail.com>
2021-06-24 15:54:31 +0700

    serve static files

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors({
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static(path.join(__dirname, '..' , 'public')));
+app.use(express.static(path.join(__dirname, '..' , 'client/build')));
 
 require("./routes/auth.route")(app);
 require("./routes/main.route")(app);
