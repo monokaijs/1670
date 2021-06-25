d683d2332f1ffd1358038846dbd841dceb0b1b0e
MonokaiJs <monokaijs@gmail.com>
2021-06-25 23:10:02 +0700

    Added edu level to config loader

--- server/controllers/public.controller.js
+++ server/controllers/public.controller.js
@@ -1,11 +1,15 @@
 const Account = require("../models/account.model");
 const Role = require("../models/role.model");
+const EduLevel = require("../models/edulevel.model");
 
 const PublicController = {
   loadConfig: async (req, res, next) => {
     const roles = await Role.find({}).select("-_id -__v");
+    const eduLevels = await EduLevel.find({}).select("-_id -__v");
+
     return res.json({
-      roles: roles
+      roles: roles,
+      eduLevels: eduLevels
     });
   }
 };
