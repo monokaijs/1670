e166908d0eb4689ae1b24d298906c0113a26c2f5
MonokaiJs <monokaijs@gmail.com>
2021-06-25 18:45:27 +0700

    Added models & load config method

--- server/controllers/manage.controller.js
+++ server/controllers/manage.controller.js
@@ -1,5 +1,28 @@
+const Account = require("../models/account.model");
+const Profile = require("../models/profile.model");
+
 const ManageController = {
   createAccount: async (req, res, next) => {
+    const fullName = req.body['full_name'];
+    const username = req.body.username;
+    const password = req.body.password;
+    const email = req.body.email;
+    const dob = req.body.dob;
+    const gender = req.body.gender;
+    const role = req.body.role;
+    const bio = req.body.bio;
+
+    const newProfile = await Profile.create({
+      fullName,
+    })
+
+    const account = await Account.create({
+      fullName,
+      username,
+      password,
+
+    })
+
   },
   createCourse: async (req, res, next) => {
   },
