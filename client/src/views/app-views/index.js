0efbcbd80d3c97687570df50933590ab2c1c97c3
Nguyen Thu Thuy <haducc246@gmail.com>
2021-06-25 17:32:41 +0700

    List Course Add Account Add Course

--- client/src/views/app-views/index.js
+++ client/src/views/app-views/index.js
@@ -2,13 +2,13 @@ import React, { lazy, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import Loading from 'components/shared-components/Loading';
 import { APP_PREFIX_PATH } from 'configs/AppConfig'
-
 export const AppViews = () => {
   return (
     <Suspense fallback={<Loading cover="content"/>}>
       <Switch>
         <Route path={`${APP_PREFIX_PATH}/home`} component={lazy(() => import(`./home`))} />
         <Route path={`${APP_PREFIX_PATH}/manage`} component={lazy(() => import(`./manage`))} />
+        <Route path={`${APP_PREFIX_PATH}/courses`} component={lazy(() => import(`./courses`))} />
         <Redirect from={`${APP_PREFIX_PATH}`} to={`${APP_PREFIX_PATH}/home`} />
       </Switch>
     </Suspense>
