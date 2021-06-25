4d4884d98f33eda2f2111ca09d36bf2b3f7bd9b3
MonokaiJs <monokaijs@gmail.com>
2021-06-25 22:58:19 +0700

    Fixed page header

--- client/src/views/app-views/courses/index.js
+++ client/src/views/app-views/courses/index.js
@@ -1,9 +1,10 @@
 import React, {lazy, Suspense} from 'react';
 import {Switch, Route} from "react-router-dom";
+import Loading from "../../../components/shared-components/Loading";
 
 const Course = ({match}) => {
   return(
-    <Suspense>
+    <Suspense fallback={<Loading cover="content"/>}>
       <Switch>
         <Route path={`${match.url}/course-list`} component={lazy(() => import(`./course-list`))} />
         <Route path={`${match.url}/my-course`} component={lazy(() => import(`./my-course`))} />
