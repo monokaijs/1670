f57df58c81ae834b9bb69d47321d23dc7bac501b
MonokaiJs <monokaijs@gmail.com>
2021-06-28 00:00:57 +0700

    fix assign course

--- client/src/views/app-views/manage/manage-category/EditCategoryForm.js
+++ client/src/views/app-views/manage/manage-category/EditCategoryForm.js
@@ -50,7 +50,7 @@ const EditCategoryForm = ({category, onAdd, visible, onClose}) => {
 		})
 	}
 	const updateCategory = (values) => {
-		ApiService.updateCourse({
+		ApiService.updateCategory({
 			category_id: category.id,
 			category_name: values.category_name,
 			code: values.code,
