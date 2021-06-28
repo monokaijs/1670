5c32edfa846d8b857cdd3962add48a071ef621c6
MonokaiJs <monokaijs@gmail.com>
2021-06-29 01:13:45 +0700

    pending search account

--- client/src/views/app-views/manage/manage-accounts/index.js
+++ client/src/views/app-views/manage/manage-accounts/index.js
@@ -1,8 +1,11 @@
 import React, {useEffect, useState} from "react";
-import {Button, Card, Input, Modal, notification, Table, Tooltip} from "antd";
+import {Button, Card, Form, Input, Modal, notification, Select, Table, Tooltip} from "antd";
 import {DeleteOutlined, EditOutlined, EyeOutlined} from "@ant-design/icons";
 import EditUserForm from "./EditUserForm";
 import ApiService from "../../../../services/ApiService";
+
+
+const {Option} = Select;
 
 const {Search} = Input;
 const {confirm} = Modal;
@@ -13,6 +16,8 @@ const ManageAccounts = () => {
 	const [onAdd, setOnAdd] = useState(false);
 	const [selectedAccount, setSelectedAccount] = useState(null);
 	const [onRender, setOnRender] = useState(false);
+	const [onSearch, setOnSearch] = useState(false);
+	const [searchAccounts, setSearchAccounts] = useState(null)
 
 	const loadAccounts = (pageSize, currentPage) => {
 		ApiService.loadAccounts({
@@ -141,27 +146,61 @@ const ManageAccounts = () => {
 			}
 		}
 	]
+
+	const onFinish = (values) => {
+		let field = values.searchField;
+		let value = values.searchValue;
+		setOnSearch(true);
+		const newAccounts = accounts.filter((account, index) => {
+			console.log(account[field])
+			return (account[field].search(value) != -1)
+		})
+		setSearchAccounts(newAccounts)
+	}
+	const [form] = Form.useForm();
 	return (
 		<>
 			<div className="search-bar mb-4 d-flex justify-content-between">
 				<Button type="primary" onClick={() => {
 					showEditForm({}, 'add')
 				}}>Create Account</Button>
-				<Input.Group>
-
-
-				</Input.Group>
-
-				<Search placeholder="Input search text" style={{width: 400}} enterButton/>
+				<div>
+					<Form form={form} onFinish={onFinish}>
+						<Input.Group compact>
+							<Form.Item
+								name='searchField'
+								noStyle
+								allowClear
+							>
+								<Select placeholder="Select...">
+									<Select.Option value="username">User Name</Select.Option>
+									<Select.Option value="fullName">Full Name</Select.Option>
+									<Select.Option value="role">Role</Select.Option>
+									<Select.Option value="eduLevel">Edu Level</Select.Option>
+									<Select.Option value="email">Email</Select.Option>
+								</Select>
+							</Form.Item>
+							<Form.Item
+								name="searchValue"
+								noStyle
+							>
+								<Input style={{width: '40%'}} placeholder="Input Value"/>
+							</Form.Item>
+							<Button type="primary" htmlType="submit">Search</Button>
+						</Input.Group>
+					</Form>
+				</div>
 			</div>
 			<Card bodyStyle={{'padding': '8px'}}>
 				<div className="table-responsive">
-					<Table columns={tableColumns} dataSource={accounts} rowKey='username'/>
+					<Table columns={tableColumns} dataSource={onSearch ? searchAccounts : accounts} rowKey='username'/>
 				</div>
 			</Card>
-			{selectedAccount &&
-			<EditUserForm onRender={onRender} setOnRender={setOnRender} account={selectedAccount} onAdd={onAdd}
-										visible={visible} onClose={closeEditForm}/>}
+			{
+				selectedAccount &&
+				<EditUserForm onRender={onRender} setOnRender={setOnRender} account={selectedAccount} onAdd={onAdd}
+											visible={visible} onClose={closeEditForm}/>
+			}
 		</>
 
 	)
