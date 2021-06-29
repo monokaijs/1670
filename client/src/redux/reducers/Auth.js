0cdad93dd58ac7f430f5288353fe46441efae6d9
Nguyen Thu Thuy <haducc246@gmail.com>
2021-06-29 10:29:37 +0700

    fix access manage page

--- client/src/redux/reducers/Auth.js
+++ client/src/redux/reducers/Auth.js
@@ -25,72 +25,75 @@ const initState = {
 }
 
 const auth = (state = initState, action) => {
-	switch (action.type) {
+  switch (action.type) {
     case AUTHENTICATED:
-			return {
-				...state,
+      console.log({
+        data: action.payload
+      })
+      return {
+        ...state,
         isLoggedIn: true,
-				loading: false,
+        loading: false,
         userInfo: action.userInfo,
-				redirect: '/',
-				token: action.token
-			}
+        redirect: '/',
+        token: action.token
+      }
     case SET_USER_INFO:
       return {
         ...state,
         userInfo: action.payload
       }
-		case SHOW_AUTH_MESSAGE:
-			return {
-				...state,
-				message: action.message,
-				showMessage: true,
-				loading: false
-			}
-		case HIDE_AUTH_MESSAGE:
-			return {
-				...state,
-				message: '',
-				showMessage: false,
-			}
-		case SIGNOUT_SUCCESS: {
-			return {
-				...state,
+    case SHOW_AUTH_MESSAGE:
+      return {
+        ...state,
+        message: action.message,
+        showMessage: true,
+        loading: false
+      }
+    case HIDE_AUTH_MESSAGE:
+      return {
+        ...state,
+        message: '',
+        showMessage: false,
+      }
+    case SIGNOUT_SUCCESS: {
+      return {
+        ...state,
         ...initState,
-				redirect: '/',
-				loading: false
-			}
-		}
-		case SIGNUP_SUCCESS: {
-			return {
-			  ...state,
-			  loading: false,
-			  token: action.token
-			}
-		}
-		case SHOW_LOADING: {
-			return {
-				...state,
-				loading: true
-			}
-		}
-		case SIGNIN_WITH_GOOGLE_AUTHENTICATED: {
-			return {
-				...state,
-				loading: false,
-				token: action.token
-			}
-		}
-		case SIGNIN_WITH_FACEBOOK_AUTHENTICATED: {
-			return {
-				...state,
-				loading: false,
-				token: action.token
-			}
-		}
-		default:
-			return state;
-	}
+        redirect: '/',
+        loading: false
+      }
+    }
+    case SIGNUP_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        token: action.token
+      }
+    }
+    case SHOW_LOADING: {
+      return {
+        ...state,
+        loading: true
+      }
+    }
+    case SIGNIN_WITH_GOOGLE_AUTHENTICATED: {
+      return {
+        ...state,
+        loading: false,
+        token: action.token
+      }
+    }
+    case SIGNIN_WITH_FACEBOOK_AUTHENTICATED: {
+      return {
+        ...state,
+        loading: false,
+        token: action.token
+      }
+    }
+    default:
+      return state;
+  }
 }
 
 export default auth
